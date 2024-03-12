7f1531d93f895c7a29cf6c13cdaf90661dcfe5f2
Bogdan Slonets <bogdan.slonets@gmail.com>
2024-03-12 21:53:28 +0200

    fix2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import {Routes, Route} from "react-router-dom";
+import Menu from "./components/Menu";
+import Privacy from "./components/Privacy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <header>
+         <h2>Home react #2</h2>
+              <Menu/>
+          </header>
+
+<main>
+    <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        <Route path="/privacy" element={<Privacy/>} />
+    </Routes>
+</main>
+      </div>
   );
 }
 
